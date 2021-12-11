31ad6d105069b96222e85cf74b40e22f34b5159d
Grotius Hasiholan <grotius.hasiholan@gmail.com>
2021-12-11 15:35:37 +0700

    Enhancing login system

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+//Controller
+const authController = require('../controllers/Auth')
+
+router.get('/login', authController.login)
+router.get('/logout', authController.logout)
+router.get('/register', authController.register)
+
+router.post('/login', authController.loginPost)
+
+module.exports = router;
--- routes/register.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//Controller
-const registerController = require('../controllers/Register')
-
-router.get('/', registerController.index)
-
-module.exports = router;
